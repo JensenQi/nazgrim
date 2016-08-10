b59b7786cf4fd37191cc9639344f704220594105
suit <suiterchik@gmail.com>
2016-08-10 22:57:43 +0800

    初始化工程目录

---
+++ mograine/src/main/java/com/nazgrim/App.java
@@ -0,0 +1,12 @@
+package com.nazgrim;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Lists.newArrayList(1, 2, 3,4 ));
+    }
+}

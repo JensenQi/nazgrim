125a2843d97fb120c51f1c324834ed0b6444349a
jinxiu.qi <suiterchik@gmail.com>
2016-08-27 16:43:37 +0800

    .

--- nazgrim/manage.py
+++ nazgrim/manage.py
@@ -2,23 +2,10 @@
 __author__ = 'jinxiu.qi'
 from flask.ext.script import Manager, Shell
 from app import create_app
-import logging
-import sys, os
-
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='[%(levelname)s]\t%(message)s',
-    datefmt='%y-%m-%d %H:%M:%S',
-    filename='log/flask.log',
-    filemode='a'
-)
 
 app = create_app()
-app.logger.addHandler(logging.getLogger())
 
 manager = Manager(app)
-
 
 if __name__ == '__main__':
     manager.run()

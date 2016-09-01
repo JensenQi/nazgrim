f847afc7f31a28ed8571a13fcb530f1994a2badb
jinxiu.qi <suiterchik@gmail.com>
2016-09-01 22:19:19 +0800

    添加登入、登出功能

--- nazgrim/app/auth/views.py
+++ nazgrim/app/auth/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, request, redirect
-from flask.ext.login import login_user
+from flask import render_template, request, redirect, flash, url_for
+from flask.ext.login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
+
 
 @auth.route('/login', methods=['POST'])
 def login():
@@ -11,4 +12,12 @@ def login():
     user = User.query.filter_by(name=user_name).first()
     if user is not None and user.verify_password(password):
         login_user(user)
+    return redirect(next_url)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    next_url = request.args.get('next')
     return redirect(next_url)

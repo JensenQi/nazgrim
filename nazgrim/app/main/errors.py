8762d5b51aa87dd8ce6c9bef2ad17a55b7eaf01b
jinxiu.qi <suiterchik@gmail.com>
2016-09-08 22:35:01 +0800

    临时fix日志记录

--- nazgrim/app/main/errors.py
+++ nazgrim/app/main/errors.py
@@ -2,13 +2,14 @@
 __author__ = 'jinxiu.qi'
 from . import nazgrim
 from flask import render_template, request
+from datetime import datetime
 import traceback
 
 @nazgrim.app_errorhandler(404)
 def not_found(e):
     exstr = traceback.format_exc()
     print exstr
-    print '请求url', request.url, '失败, 返回404'
+    print datetime.now(), request.url, '请求url', request.url, '失败, 返回404'
     return render_template('error.html', error_code='404'), 404
 
 

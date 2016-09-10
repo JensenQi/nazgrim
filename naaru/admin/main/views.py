dbf9fb101df38ad08c38a920bd4d8427de2732f1
jinxiu.qi <suiterchik@gmail.com>
2016-09-11 02:25:18 +0800

    测试nginx分发

--- naaru/admin/main/views.py
+++ naaru/admin/main/views.py
@@ -3,4 +3,8 @@ from . import admin
 
 @admin.route('/')
 def home():
-    return '<h1>Welcome to Naaru</h1>'
+    return '<h1>Welcome to Naaru</h1><br><a href="/login">login</a>'
+
+@admin.route('/login')
+def login():
+    return '<h1>login</h1>'

74117037c9c6edc25a2275fd71c03412d413fb58
jinxiu.qi <suiterchik@gmail.com>
2016-09-11 00:04:05 +0800

    更新配置文件读取

--- nazgrim/config.py
+++ nazgrim/config.py
@@ -2,6 +2,6 @@
 __author__ = 'jinxiu.qi'
 import ConfigParser
 cf = ConfigParser.ConfigParser()
-cf.read('config.ini')
+cf.read('config/app.ini')
 DATABASE_URI = cf.get('db', 'URI')
 SECRET_KEY = cf.get('secret', 'key')

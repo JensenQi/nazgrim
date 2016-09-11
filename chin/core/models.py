0c9910fb343f1cd5985dbab820e8e8cf9cc273db
jinxiu.qi <suiterchik@gmail.com>
2016-09-12 01:03:06 +0800

    修改数据表结构

--- chin/core/models.py
+++ chin/core/models.py
@@ -1,28 +1,52 @@
 # coding=utf-8
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Text, Boolean, Time, String, DateTime, SmallInteger, ForeignKey
+from sqlalchemy import Column, Integer, Text, Boolean, String, DateTime, SmallInteger, ForeignKey, Enum, TypeDecorator
+import enum, json
 
 BaseModel = declarative_base()
 
 
+class Json(TypeDecorator):
+    impl = Text
+
+    def process_bind_param(self, value, dialect):
+        return json.dumps(value)
+
+    def process_result_value(self, value, dialect):
+        return json.loads(value)
+
+
 class Task(BaseModel):
-    __tablename__ = 'tasks'
-    id = Column(Integer, primary_key=True)
-    command = Column(Text)
-    args = Column(Text)
-    valid = Column(Boolean, index=True)
-    scheduled_time = Column(Time)
-    # todo: 优先级
-    # todo: 机器池
-    # todo: 创建者
+    # 基础
+    __tablename__ = 'task'
+    id = Column(Integer, primary_key=True, doc="任务id")
+    user = Column(String(32), doc="任务创建者")
+    group = Column(String(32), doc="任务所属组")
+    create_time = Column(DateTime, doc="任务创建时间")
+
+    # 执行相关
+    command = Column(Text, doc="任务执行命令")
+    args = Column(Text, doc="任务执行参数")
+    priority = Column(SmallInteger, doc="任务优先级")
+    machine_pool = Column(Json, doc="机器池list")
+
+    # 调度相关
+    valid = Column(Boolean, index=True, doc="是否调度")
+    scheduled_type = Column(Enum('once', 'day', 'week', 'month', 'year'), index=True, doc="调度频率")
+    year = Column(SmallInteger, doc="调度时间-年")
+    month = Column(SmallInteger, doc="调度时间-月")
+    day = Column(SmallInteger, doc="调度时间-日")
+    hour = Column(SmallInteger, doc="调度时间-时")
+    minute = Column(SmallInteger, doc="调度时间-分")
+    second = Column(SmallInteger, doc="调度时间-秒")
 
 
-class ExcLog(BaseModel):
-    __tablename__ = 'exc_logs'
-    id = Column(Integer, primary_key=True)
-    task_id = Column(Integer, ForeignKey('tasks.id'))
-    version = Column(String(14))
-    pooled_time = Column(DateTime)
-    begin_time = Column(DateTime)
-    finish_time = Column(DateTime)
-    status = Column(SmallInteger, index=True)
+class TaskQueue(BaseModel):
+    __tablename__ = 'task_queue'
+    id = Column(Integer, primary_key=True, doc="日志id")
+    task_id = Column(Integer, ForeignKey('task.id'), doc='任务id')
+    version = Column(String(14), doc='版本号')
+    pooled_time = Column(DateTime, doc='入池时间')
+    begin_time = Column(DateTime, doc='开始执行时间')
+    finish_time = Column(DateTime, doc='执行结束时间')
+    status = Column(Enum('waiting', 'running', 'failed', 'killing', 'repairing'), index=True, doc='状态')

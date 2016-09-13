5d406c21369e7defd10c92b9a9a680ea4338b45b
jinxiu.qi <suiterchik@gmail.com>
2016-09-13 23:33:56 +0800

    修改taskmeta接口

--- chin/core/master/TaskMeta.py
+++ chin/core/master/TaskMeta.py
@@ -1,7 +1,5 @@
 from ..models import Task
-from .. import DBSession
 from VersionController import VersionController
-
 
 class TaskMeta:
 
@@ -9,27 +7,21 @@ class TaskMeta:
         pass
 
     @staticmethod
-    def add(task):
-        session = DBSession()
+    def add(task, session):
         session.add(task)
         VersionController.handle_add(task, session)
         session.commit()
-        session.close()
 
     @staticmethod
-    def remove(task):
-        session = DBSession()
+    def remove(task, session):
         task = session.query(Task).filter(Task.id == task.id).first()
         task.valid = False
         session.add(task)
         VersionController.handle_remove(task, session)
         session.commit()
-        session.close()
 
     @staticmethod
-    def update(task):
-        session = DBSession()
+    def update(task, session):
         session.add(task)
         VersionController.handle_update(task, session)
         session.commit()
-        session.close()

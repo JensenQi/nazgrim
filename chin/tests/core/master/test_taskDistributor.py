2452300d716f2a67d258bf2d51005e15b0727386
jinxiu.qi <suiterchik@gmail.com>
2016-09-18 23:41:39 +0800

    补充单测

--- chin/tests/core/master/test_taskDistributor.py
+++ chin/tests/core/master/test_taskDistributor.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import os
 
 os.chdir('../../../')
@@ -24,26 +25,98 @@ class TestTaskDistributor(TestCase):
         BaseModel.metadata.drop_all(engine)
         BaseModel.metadata.create_all(engine)
 
-        task = Task(
+        rerun_task = Task(
                 user='root',
-                command='shell /home/suit/day_task.sh',
+                command='shell /home/suit/rerun_task.sh',
                 machine_pool=['slave_1', 'slave_2', 'slave_3'],
                 valid=True,
+                rerun=True,
+                rerun_times=2,
                 scheduled_type='day',
                 hour=10, minute=11, second=12
         )
 
-        TaskMeta.add(task).by(self.session)
+        unrerun_task = Task(
+                user='root',
+                command='shell /home/suit/rerun_task.sh',
+                machine_pool=['slave_4', 'slave_5', 'slave_6'],
+                valid=True,
+                scheduled_type='week',
+                weekday=datetime.now().isoweekday(),
+                hour=18, minute=19, second=20
+        )
+
+        TaskMeta.add(rerun_task).by(self.session)
+        TaskMeta.add(unrerun_task).by(self.session)
         self.session.commit()
 
     def tearDown(self):
         self.session.close()
 
     def test_serve(self):
-        i = self.count_down
+        # 启动版本号服务
         self.version_controller.serve()
+        time.sleep(2)
+
+        rerun_task = self.session.query(TaskQueue).filter_by(id=1).first()
+        unrerun_task = self.session.query(TaskQueue).filter_by(id=2).first()
+
+
+        # 确认任务未分配
+        self.assertEqual(rerun_task.status, None)
+        self.assertEqual(rerun_task.execute_machine, None)
+        self.assertEqual(rerun_task.run_count, 0)
+        self.assertEqual(unrerun_task.status, None)
+        self.assertEqual(unrerun_task.execute_machine, None)
+        self.assertEqual(unrerun_task.run_count, 0)
+
+        # 启动任务分发服务
         self.task_distributor.serve()
-        while i > 0:
-            i -= 1
-            print self.version_controller.is_live(), self.task_distributor.is_live()
-            time.sleep(1)
+        time.sleep(2)
+
+        # 确认任务分发成功
+        self.session.commit()
+        self.assertNotEqual(rerun_task.execute_machine, None)
+        self.assertEqual(rerun_task.status, 'waiting')
+        self.assertEqual(rerun_task.run_count, 1)
+        self.assertNotEqual(unrerun_task.execute_machine, None)
+        self.assertEqual(unrerun_task.status, 'waiting')
+        self.assertEqual(unrerun_task.run_count, 1)
+
+        # 测试可重跑任务
+        # 第一次: slave放弃执行
+        rerun_task.status = 'abandon'
+        self.session.add(rerun_task)
+        self.assertEqual(rerun_task.status, 'abandon')
+        self.session.commit()
+        time.sleep(2)
+        self.assertEqual(rerun_task.status, 'waiting')
+        self.assertEqual(rerun_task.run_count, 2)
+
+        # 第二次: 执行失败
+        rerun_task.status = 'failed'
+        self.session.add(rerun_task)
+        self.assertEqual(rerun_task.status, 'failed')
+        self.session.commit()
+        time.sleep(2)
+        self.assertEqual(rerun_task.status, 'waiting')
+        self.assertEqual(rerun_task.run_count, 3)
+
+        # 第三次: 超出重试次数
+        rerun_task.status = 'failed'
+        self.session.add(rerun_task)
+        self.assertEqual(rerun_task.status, 'failed')
+        self.session.commit()
+        time.sleep(2)
+        self.assertEqual(rerun_task.status, 'failed')
+        self.assertEqual(rerun_task.run_count, 3)
+
+        # 测试不可重跑任务
+        unrerun_task.status = 'failed'
+        self.session.add(unrerun_task)
+        self.assertEqual(unrerun_task.status, 'failed')
+        self.session.commit()
+        time.sleep(2)
+        self.assertEqual(unrerun_task.status, 'failed')
+        self.assertEqual(unrerun_task.run_count, 1)
+

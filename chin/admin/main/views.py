43c4372f9890ebbf536ea5483dc951308d441a1e
jinxiu.qi <suiterchik@gmail.com>
2016-09-25 03:43:23 +0800

    添加执行日志demo

--- chin/admin/main/views.py
+++ chin/admin/main/views.py
@@ -6,6 +6,12 @@ from flask import render_template
 def home():
     return render_template('index.html')
 
+
+@admin.route('/routine_log')
+def routine_log():
+    return render_template('routine_log.html')
+
+
 @admin.route('/login')
 def login():
     return '<h1>login</h1>'

4c0670099e9458b2e44af3b3ca4147cc9261e8f9
jinxiu.qi <suiterchik@gmail.com>
2016-09-24 23:13:51 +0800

    更改启动方式

--- chin/chin.py
+++ chin/chin.py
@@ -1,3 +1,9 @@
+# coding=utf-8
 import core
+import sys
 
-core.run('master')
+if __name__ == '__main__':
+    if len(sys.argv) is not 2 or sys.argv[1] not in ('slave', 'master'):
+        raise Exception('启动错误,　请使用 " /path/to/python /path/to/chin.py master或slave " 启动')
+    role = sys.argv[1]
+    core.run(role)
